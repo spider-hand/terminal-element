c677fc4d13160934a71a11d095ba07b03062073c
spider-hand <creative.spider.hand@gmail.com>
2026-03-21 16:30:47 +0900

    chore: set up Storybook

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
@@ -6,20 +9,14 @@ import { defineConfig } from "eslint/config";
 import eslintPluginLit from "eslint-plugin-lit";
 import eslintConfigPrettier from "eslint-config-prettier";
 
-export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser },
-  },
-  tseslint.configs.recommended,
-  eslintPluginLit.configs["flat/recommended"],
-  eslintConfigPrettier,
-  {
-    files: ["**/*.css"],
-    plugins: { css },
-    language: "css/css",
-    extends: ["css/recommended"],
-  },
-]);
+export default defineConfig([{
+  files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+  plugins: { js },
+  extends: ["js/recommended"],
+  languageOptions: { globals: globals.browser },
+}, tseslint.configs.recommended, eslintPluginLit.configs["flat/recommended"], eslintConfigPrettier, {
+  files: ["**/*.css"],
+  plugins: { css },
+  language: "css/css",
+  extends: ["css/recommended"],
+}, ...storybook.configs["flat/recommended"]]);
